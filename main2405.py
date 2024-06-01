fcd4b0fae2e7107336595639ecac8da45262e6e3
hutch <hutch@naver.com>
2024-06-02 08:23:26 +0900

    매도예약 시 self.balance 미반영 이슈-프로그램 반복시 최신화 처리

--- main2405.py
+++ main2405.py
@@ -10,15 +10,13 @@ pd.set_option('display.float_format', lambda x: f'{x:,.2f}')
 
 class autoTrade :
     def __init__(self, start_cash, ticker) :
-        self.fee_rate = 0.0005 # 수수료
+        self.fee_rate = 0.0005 # 수수료 0.05%
         self.target_price = 0 # 목표 매수가
         # self.bull = False # 상승장 여부
         self.ma5 = 0 # 5일 이동평균
         self.ticker = ticker # 티커
-        # self.buy_yn = False # 매수 여부
-        self.balance = upbit.get_balance(self.ticker)
-        # self.buy_yn = (upbit.get_balance(self.ticker) > 0) #보유 여부-매도 예약 후 미체결 등으로 보유여부는 바뀔 수 있으므로 최신화 필요
-
+        self.balance = upbit.get_balance(self.ticker) #매도 예약 시에도 보유수량이 0으로 초기화 되지 않는 오류가 있음
+        
         self.start_cash = start_cash # 시작 자산
 
         self.timer = 0
@@ -29,10 +27,11 @@ class autoTrade :
         now = datetime.datetime.now() # 현재 시간
 
         slackBot.message("프로그램 시작 시간 : " 
-                         + str(now.strftime('%m.%d - %H:%M:%S')) 
+                         + str(now.strftime('%m/%d - %H:%M:%S')) 
                          + "\n매매 대상 : " + self.ticker 
                          + "\n시작 원화 자산 : " + str(f"{self.start_cash:,.0f} 원")
                          + "\n보유 평단 : " + str(f"{upbit.get_avg_buy_price(self.ticker):,.0f} 원")
+                         + "\t보유 수량 : " + str(f"{self.balance:,.4f}"),
                          )
         
         openTime = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(days=1, hours=9, seconds=10) # 09:00:10
@@ -42,30 +41,32 @@ class autoTrade :
                 now = datetime.datetime.now()
                 current_price = pyupbit.get_current_price(self.ticker)
                 avg_buy_price = upbit.get_avg_buy_price(self.ticker) #평단 조회
+                balance = upbit.get_balance(self.ticker) # 매도 예약시 보유수량 0 반영을 위한 수량 초기화
 
                 # 구동 메세지 출력
                 if(self.timer % 60 == 0) :
-                    print(now.strftime('%m.%d - %H:%M:%S'),
-                          "\topenTime :", openTime.strftime('%m.%d - %H:%M:%S'), 
-                          "\tTarget :", f"{self.target_price:,.0f}", 
+                    print(now.strftime('%m/%d - %H:%M:%S'),
+                          "\t매도예정 :", openTime.strftime('%m/%d - %H:%M:%S'), 
+                          "\t목표가 :", f"{self.target_price:,.0f}", 
                           "\t현재가 :", f"{current_price:,.0f}",
                           "\t평단 :", f"{avg_buy_price:,.0f}",
-                          "\t보유수량 :", self.balance,
-                          "\t현재수익률 :", f"{(current_price - avg_buy_price)/avg_buy_price:.2%}" ,
-                          "\tMA5 :", f"{self.ma5:,.0f}",
+                          "\t보유수량 :", f"{balance:,.4f}",
+                          "\t수익률 :", f"{(current_price*(1-self.fee_rate) - avg_buy_price) / avg_buy_price:.2%}" ,
+                          "\t수익금 :", f"{(current_price*(1-self.fee_rate) - avg_buy_price ) * balance :,.0f}" ,
+                          "\tMA(5) :", f"{self.ma5:,.0f}",
                           )
                 
                 # 매일 오전 9시 10초에 매도
                 if openTime < now < openTime + datetime.timedelta(seconds=5) :
                     openTime = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(days=1, hours=9, seconds=10)
-                    if self.balance > 0 :# 해당 코인을 보유하고 있을 경우
+                    if balance > 0 :# 해당 코인을 보유하고 있을 경우
                         print("==================== [ 매도 시도 ] ====================")
                         slackBot.message("매도 시도")
                         self.sell_coin()
                     self.get_today_data() # 데이터 갱신
                 
                 # 현재 가격이 목표가 이상이고, 5일 이동평균 이상이면서, 보유수량이 없으면 매수
-                if( (current_price >= self.target_price) and (current_price >= self.ma5) and (self.balance==0) ) : # 매수 시도
+                if( (current_price >= self.target_price) and (current_price >= self.ma5) and (balance==0) ) : # 매수 시도
                     print("==================== [ 매수 시도 ] ====================")
                     slackBot.message("매수 시도")
                     self.buy_coin()
@@ -112,7 +113,7 @@ class autoTrade :
         print("==================== [ 데이터 갱신 완료 ] ====================\n")
 
     def buy_coin(self) :
-        balance = upbit.get_balance() # 잔고 조회
+        balance = upbit.get_balance() # 원화 잔고 조회
         
         if balance > 5000 : # 잔고 5000원 이상일 때
             upbit.buy_market_order(self.ticker, balance * 0.9995)
@@ -122,7 +123,7 @@ class autoTrade :
             slackBot.message("#매수 주문\n매수 주문 가격 : " + str(buy_price) + "원")
 
     def sell_coin(self) :
-        balance = upbit.get_balance(self.ticker) # 잔고 조회
+        balance = upbit.get_balance(self.ticker) # 코인 수량 조회
 
         upbit.sell_market_order(ticker, balance)
 
